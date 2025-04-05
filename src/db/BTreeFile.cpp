29f7ea5e7945d7a9be24d0aa52240808a3e10036
annet <anneturmel7@gmail.com>
2025-04-04 22:41:57 -0400

    fin pa2

--- src/db/BTreeFile.cpp
+++ src/db/BTreeFile.cpp
@@ -1,4 +1,6 @@
+#include <cmath>
 #include <cstring>
+#include <memory_resource>
 #include <db/BTreeFile.hpp>
 #include <db/Database.hpp>
 #include <db/IndexPage.hpp>
@@ -12,6 +14,62 @@ BTreeFile::BTreeFile(const std::string &name, const TupleDesc &td, size_t key_in
 
 void BTreeFile::insertTuple(const Tuple &t) {
     // TODO pa2
+    if (!td.compatible(t)) {
+        throw std::runtime_error("Tuple not compatible with TupleDesc");
+    }
+
+    // get root
+    BufferPool &bufferPool = getDatabase().getBufferPool();
+    PageId pid{name, 0};
+    Page &p = bufferPool.getPage(pid);
+
+    // keep track of pages seen so splitting can occur as necessary
+    IndexPage* page_path[(int)std::log(numPages)];
+    size_t page_nums[(int)std::log(numPages)];
+    int num_pages_seen = 0;
+    IndexPage ip(p);
+    page_path[num_pages_seen] = &ip;
+    page_nums[num_pages_seen] = 0;
+    if (ip.header->size != 0) {
+        int search_key = std::get<int>(t.get_field(key_index));
+        int search_guide;
+        do {
+            for (search_guide = 0; search_guide < page_path[num_pages_seen]->header->size; search_guide++) {
+                if (page_path[num_pages_seen]->keys[search_guide] <= search_key) {
+                    break;
+                }
+            }
+            if (page_path[num_pages_seen]->header->index_children) {
+                num_pages_seen++;
+                page_nums[num_pages_seen] = page_path[num_pages_seen - 1]->children[search_guide + 1];
+                IndexPage child(bufferPool.getPage(PageId{name, page_nums[num_pages_seen]}));
+                page_path[num_pages_seen] = &child;
+            } else {
+                break;
+            }
+        } while (true);
+        LeafPage child(bufferPool.getPage(PageId{name, page_path[num_pages_seen]->children[search_guide + 1]}), td, key_index);
+        if (child.insertTuple(t)) {
+            db::Page new_leaf_page{};
+            db::LeafPage new_leaf{new_leaf_page, td, key_index};
+            int new_key = child.split(new_leaf);
+            if (page_path[num_pages_seen]->insert(new_key, page_path[num_pages_seen]->children[search_guide])) {
+                while (num_pages_seen > 0) {
+                    db::Page new_page{};
+                    db::IndexPage new_index{new_page};
+                    new_key = page_path[num_pages_seen]->split(new_index);
+                    if (!page_path[num_pages_seen - 1]->insert(new_key, page_nums[num_pages_seen])) {
+                        break;
+                    }
+                    num_pages_seen--;
+                }
+                if (num_pages_seen <= 0) {
+                    db::Page new_root_page{};
+                    db::IndexPage new_root{new_root_page};
+                }
+            }
+        }
+    }
 }
 
 void BTreeFile::deleteTuple(const Iterator &it) {
@@ -20,16 +78,72 @@ void BTreeFile::deleteTuple(const Iterator &it) {
 
 Tuple BTreeFile::getTuple(const Iterator &it) const {
     // TODO pa2
+    BufferPool &bufferPool = getDatabase().getBufferPool();
+    PageId pid{name, it.page};
+    Page &p = bufferPool.getPage(pid);
+    LeafPage lp(p, td, key_index);
+    return lp.getTuple(it.slot);
 }
 
 void BTreeFile::next(Iterator &it) const {
     // TODO pa2
+    BufferPool &bufferPool = getDatabase().getBufferPool();
+    if (it.page != 0) {
+        PageId pid{name, it.page};
+        Page &p = bufferPool.getPage(pid);
+        const LeafPage lp(p, td, key_index);
+        it.slot++;
+        if (it.slot <= lp.header->size) {
+            return;
+        }
+        it.page = lp.header->next_leaf;
+    }
+    while (it.page != 0) {
+        PageId pid{name, it.page};
+        Page &p = bufferPool.getPage(pid);
+        const LeafPage lp(p, td, key_index);
+        it.slot = 0;
+        if (it.slot <= lp.header->size) {
+            return;
+        }
+        it.page = lp.header->next_leaf;
+    }
 }
 
 Iterator BTreeFile::begin() const {
     // TODO pa2
+    BufferPool &bufferPool = getDatabase().getBufferPool();
+    PageId pid{name, 0};
+    Page &p = bufferPool.getPage(pid);
+    IndexPage ip(p);
+    if (ip.header->size != 0) {
+        do {
+            if (ip.header->index_children) {
+                IndexPage child(bufferPool.getPage(PageId{name, ip.children[0]}));
+                ip = child;
+            } else {
+                return {*this, ip.children[0], 0};
+            }
+        } while (true);
+    }
+    return {*this, 0, 0};
 }
 
 Iterator BTreeFile::end() const {
     // TODO pa2
+    BufferPool &bufferPool = getDatabase().getBufferPool();
+    PageId pid{name, 0};
+    Page &p = bufferPool.getPage(pid);
+    IndexPage ip(p);
+    if (ip.header->size != 0) {
+        do {
+            if (ip.header->index_children) {
+                IndexPage child(bufferPool.getPage(PageId{name, ip.children[ip.header->size - 1]}));
+                ip = child;
+            } else {
+                return {*this, ip.children[ip.header->size], 0};
+            }
+        } while (true);
+    }
+    return {*this, 0, 0};
 }
--- src/db/IndexPage.cpp
+++ src/db/IndexPage.cpp
@@ -1,16 +1,49 @@
 #include <db/IndexPage.hpp>
 #include <stdexcept>
+#include <cstring>
 
 using namespace db;
 
 IndexPage::IndexPage(Page &page) {
     // TODO pa2
+    header = (IndexPageHeader *)page.data();
+    capacity = (DEFAULT_PAGE_SIZE - sizeof(IndexPageHeader) - sizeof(size_t)) / (sizeof(size_t) + sizeof(int));
+    children = reinterpret_cast<size_t *>(page.data() + DEFAULT_PAGE_SIZE - (capacity + 1) * sizeof(size_t));
+    keys = reinterpret_cast<int *>(children - (capacity * sizeof(int)));
 }
 
 bool IndexPage::insert(int key, size_t child) {
     // TODO pa2
+    int index = 0;
+    while (index < header->size && keys[index] < key) {
+        index++;
+    }
+    if (index < header->size && keys[index] > key) { // move subsequent keys to make room for this one
+        int num_to_move = header->size - index + 1;
+        std::memmove(&keys[index + 1], &keys[index], num_to_move * sizeof(int));
+        std::memmove(&children[index + 1], &children[index], (num_to_move + 1) * sizeof(int));
+    }
+    keys[index] = key;
+    children[index + 1] = child;
+    header->size++;
+    if (header->size >= capacity) {
+        return true;
+    }
+    return false;
 }
 
 int IndexPage::split(IndexPage &new_page) {
     // TODO pa2
+    size_t first_half = header->size / 2;
+    size_t second_half = header->size - first_half;
+    new_page.header->index_children = header->index_children;
+    int *second_half_keys = keys + first_half;
+    unsigned long *second_half_children = children + first_half;
+    int new_key = second_half_keys[0];
+    second_half_keys++;
+    std::memmove(new_page.keys, second_half_keys, second_half * sizeof(int));
+    std::memmove(new_page.children, second_half_children, second_half + 1);
+    new_page.header->size = second_half - 1;
+    header->size = first_half;
+    return new_key;
 }
